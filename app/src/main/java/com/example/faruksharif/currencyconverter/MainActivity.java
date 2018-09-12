576f0bd565f09426b89a08a154550ec335ab7a71
Faruk Sharif <md.faruksharif@gmail.com>
2018-09-12 14:26:51 +0600

    Initial commit

---
+++ app/src/main/java/com/example/faruksharif/currencyconverter/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.faruksharif.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void Onclick(View view){
+
+        EditText txt1=(EditText) findViewById(R.id.txt1);
+        Log.i("Currency!!!","Please Click!!!");
+        Log.i("Values1",txt1.getText().toString());
+        int DollarValueInInt = Integer.parseInt(txt1.getText().toString());
+        int DollarValueInBDT = DollarValueInInt*64;
+        String Total= Integer.toString(DollarValueInBDT);
+        Toast.makeText(this, "Cad amount "+txt1.getText().toString()+" $ to BDT is "+ Total, Toast.LENGTH_LONG).show();
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
